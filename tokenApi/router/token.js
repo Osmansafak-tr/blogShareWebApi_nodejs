478203e4ac373f6873dfde9385ddd2591f945568
Osmansafak-tr <osmansfk53@gmail.com>
2023-07-03 22:43:24 +0300

    tokenApi | tryCatch util added

--- tokenApi/router/token.js
+++ tokenApi/router/token.js
@@ -1,13 +1,14 @@
 const router = require("express").Router();
 const validator = require("../validator/token");
 const { handleValResult } = require("../middlewares/index");
+const { tryCatch } = require("../utils");
 const controller = require("../controller/token");
 
 router.post(
   "/connect/token",
   validator.CreateTokensValidator,
   handleValResult,
-  controller.CreateTokens
+  tryCatch(controller.CreateTokens)
 );
 
 module.exports = router;
